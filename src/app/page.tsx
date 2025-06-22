180b3d731643fd12c64a55b03d1baf051e1ccfe6
Michal <hello@lewandowskimichal.tech>
2025-06-22 23:52:05 +0200

    add homepage stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,13 @@ import Links from '@/components/links'
 export default function Home() {
   return (
     <div className="font-base">
-      <h1 className="text-2xl font-heading sm:text-4xl">John Doe</h1>
-      <p className="mt-2 text-lg sm:text-xl">Software Engineer</p>
+      <h1 className="text-2xl font-heading sm:text-4xl">Michał Lewandowski</h1>
+      <p className="mt-2 text-lg sm:text-xl">Data Engineer</p>
       <div className="mt-8 text-base sm:text-lg">
-        <p>Hi, my name is John Doe, software engineer based in NYC.</p>
+        <p>Hi, my name is Michał Lewandowski, Data Engineer based in Poznań, Poland.</p>
 
         <br />
 
-        <p>
-          This is the neobrutalism portfolio template. Check the{' '}
-          <a
-            target="_blank"
-            className="font-heading underline"
-            href="https://github.com/neobrutalism-templates/portfolio"
-          >
-            github repo
-          </a>{' '}
-          for more info.
-        </p>
       </div>
 
       <Links />
