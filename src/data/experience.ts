2c6d34bc1b39b0d4a49b9a3dda8c8536bdf49ee6
Michal <hello@lewandowskimichal.tech>
2025-06-23 00:15:23 +0200

    add base experience

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -5,30 +5,30 @@ const PAST_ROLES: {
   startDate: string
   endDate: string
 }[] = [
-  {
-    company: 'ACME Solutions',
-    role: 'CEO',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam vel hic aspernatur molestiae exercitationem rerum reiciendis mollitia magnam harum ipsum?',
-    startDate: 'March 2023',
-    endDate: 'Now',
-  },
-  {
-    company: 'Google',
-    role: 'Software Engineer',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam vel hic aspernatur molestiae exercitationem rerum reiciendis mollitia magnam harum ipsum?',
-    startDate: 'April 2021',
-    endDate: 'March 2023',
-  },
-  {
-    company: 'Vercel',
-    role: 'Developer Advocate',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam vel hic aspernatur molestiae exercitationem rerum reiciendis mollitia magnam harum ipsum?',
-    startDate: 'April 2021',
-    endDate: 'June 2018',
-  },
-]
+    {
+      company: 'Lingaro',
+      role: '(Lead) BI Engineer/Analytics Engineer/BI Fullstack Developer',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam vel hic aspernatur molestiae exercitationem rerum reiciendis mollitia magnam harum ipsum?',
+      startDate: 'January 2024',
+      endDate: 'Now',
+    },
+    {
+      company: 'Lingaro',
+      role: 'PowerBI Developer',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam vel hic aspernatur molestiae exercitationem rerum reiciendis mollitia magnam harum ipsum?',
+      startDate: 'August 2022',
+      endDate: 'Novebmer 2022',
+    },
+    {
+      company: 'GlaxoSmithKline/Haleon',
+      role: 'PowerBI Developer',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam vel hic aspernatur molestiae exercitationem rerum reiciendis mollitia magnam harum ipsum?',
+      startDate: 'April 2021',
+      endDate: 'July 2022',
+    },
+  ]
 
 export default PAST_ROLES
